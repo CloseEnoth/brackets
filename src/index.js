f180ba5ab2d38540c735193e5ea37bb493ba80e4
CloseEnoth <den.prazdnikov2020@gmail.com>
2022-07-24 19:29:15 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0) return false;
+
+  let newArr = []
+  let arrItem = bracketsConfig.map(item => item.join('')).reduce((sum, el) => sum.concat(el), []);
+
+  let obj = Object.assign({}, arrItem);
+
+  /*   for (let i = 0; i < str.length / 2; i++) {
+    newArr.push(str[i]);
+    newArr.push(str[str.length - i - 1])
+  } */
+
+
+  newArr = str.split('');
+  for (let i = 0; i < newArr.length / 2; i++) {
+    for (let key in obj) {
+      if (`${newArr[i]}${newArr[newArr.length - i - 1]}` === `${obj[key]}`) return true
+    }
+    return false;
+  }
+
 }
